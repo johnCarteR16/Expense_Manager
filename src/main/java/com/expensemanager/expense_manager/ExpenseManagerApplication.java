590dee3ebdc849ac7308cb744ab5449b24ff0ce1
johnCarteR16 <nlsnrjlinux1141@gmail.com>
2022-09-04 21:00:20 +0530

    initialised files

---
+++ src/main/java/com/expensemanager/expense_manager/ExpenseManagerApplication.java
@@ -0,0 +1,15 @@
+package com.expensemanager.expense_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseManagerApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        SpringApplication.run(ExpenseManagerApplication.class, args);
+        System.out.println("Hello World");
+    }
+
+}
